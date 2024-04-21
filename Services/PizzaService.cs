9894682e0093ff1af2274c9d6916226eb8498b1d
trsaints <thiago.dev2k3@gmail.com>
2024-04-21 08:24:17 -0300

    feat: add PizzaService

---
+++ Services/PizzaService.cs
@@ -0,0 +1,48 @@
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new(1, "Classic Italian", false),
+            new(2, "Veggie", true)
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        
+        if (pizza is null) 
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+
+        if (index is -1)
+            return;
+        
+        Pizzas[index] = pizza;
+    }
+}
